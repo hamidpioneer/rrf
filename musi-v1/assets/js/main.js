527d539dd1619893e0b62ef82f01f4669f97cf4e
Hamid Pioneer <hamidpioneer@yahoo.com>
2017-12-08 20:23:05 +0600

    musi-v1 uploaded
    musi-v1 uploaded

---
+++ musi-v1/assets/js/main.js
@@ -0,0 +1,20 @@
+(function($){
+  "use strict";
+
+  jQuery(document).ready(function($){
+
+    $('.slide-items').owlCarousel({
+      items: 1,
+      nav: false,
+      dots: true,
+      loop: true,
+      autoplay: false,
+    });
+
+
+  });
+
+  jQuery(window).load(function(){
+
+  });
+})(jQuery);
