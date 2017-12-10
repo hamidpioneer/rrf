962a03b2f0dac1dd6c2c0db2d9bff93a4b69143e
Hamid Pioneer <hamidpioneer@yahoo.com>
2017-12-10 19:03:46 +0600

    Class 22 uploaded + link corrected
    class22-Team Section

---
+++ class22/assets/js/main.js
@@ -0,0 +1,20 @@
+(function($){
+  "use strict";
+
+  jQuery(document).ready(function($){
+
+    $('.homepage-slides').owlCarousel({
+      items: 1,
+      nav: false,
+      dots: true,
+      loop: true,
+      autoplay: true,
+    });
+
+
+  });
+
+  jQuery(window).load(function(){
+
+  });
+})(jQuery);
