94a241367ab6edd89f91efa186f1b35ea2fa9a44
hamidpioneer <hamidpioneer@yahoo.com>
2017-12-24 08:58:16 +0600

    class 31 uploaded
    class 31 uploaded

---
+++ class31/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+			/* start skrollr */
+      var s = skrollr.init();
+			s.refresh($('#parallax-1'));
+			/* end skrollr */
+
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+
+    });
+
+
+}(jQuery));
