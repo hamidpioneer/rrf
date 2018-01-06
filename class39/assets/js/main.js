55af5f3b5832c9157de3a052e3e180b08dcb4935
hamidpioneer <hamidpioneer@yahoo.com>
2018-01-07 01:22:06 +0600

    class 39 uploaded
    class 39 uploaded

---
+++ class39/assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+			$(".hamburger-icon").on("click", function(){
+				$(".mobile-menu, .mobile-menu-overlay").addClass("active");
+				return false;
+			});
+
+			$(".mobile-menu-close-icon, .mobile-menu-overlay").on("click", function(){
+				$(".mobile-menu, .mobile-menu-overlay").removeClass("active");
+			});
+    });
+
+
+    jQuery(window).load(function(){
+    });
+
+
+}(jQuery));
